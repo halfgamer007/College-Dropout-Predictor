c3cdaf9db859d68fb8054161168a360783e28f24
halfgamer007 <42691987+halfgamer007@users.noreply.github.com>
2018-08-25 17:26:54 +0530

    Add files via upload

---
+++ predictor/Login.sql
@@ -0,0 +1,11 @@
+Create Table Login
+(
+	Username varchar(30),
+	Password varchar(30)
+);
+
+Insert into Login values('Abhishek','Abhi@123');
+Insert into Login values('Chinmay','Chinmay@123');
+Insert into Login values('Shreyas','Shreyas@123');
+Insert into Login values('Sahil','Sahil@123');
+Insert into Login values('Vyas','Vyas@123');
